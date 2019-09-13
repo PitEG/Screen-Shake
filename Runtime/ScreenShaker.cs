460f373280c26834e8232ddb41a54a1080fe2a84
PitEG <enriqueiigan@gmail.com>
2019-09-13 03:07:44 -0700

    attempted it but it's not working very well

---
+++ Runtime/ScreenShaker.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PitGan.ScreenShake {
+	public class ScreenShaker : MonoBehaviour {
+
+		/// <summary>
+		/// number of frames until shaking stops
+		/// </summary>
+		private float shakeTimer;
+
+		/// <summary>
+		/// the number of frames it waits before shaking
+		/// </summary>
+		private int shakeVigor = 30;
+
+		private int radius;
+		private static int FullCircleInDegrees = 360;
+
+		public int ShakeVigor {
+			get { return shakeVigor; }
+			set { shakeVigor = value; }
+		}
+
+		private void OnPreRender() {
+			if (shakeTimer > 0 && shakeTimer % shakeVigor == 0) {
+				shakeTimer--;
+				MoveCamera();
+			}
+		}
+
+		private void MoveCamera() {
+			Vector2 newPos = Random.insideUnitCircle * radius;
+			transform.localPosition += (Vector3)newPos;
+		}
+
+		public void Shake(int shakeTime, int shakeRadius) {
+			this.shakeTimer = shakeTime;
+			this.radius = shakeRadius;
+		}
+	}
+}
