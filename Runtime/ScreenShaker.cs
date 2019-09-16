d5416fce5ff094ecd023e50d36d3a14166556720
PitEG <enriqueiigan@gmail.com>
2019-09-15 17:08:21 -0700

    Should work now but needs better structure

--- Runtime/ScreenShaker.cs
+++ Runtime/ScreenShaker.cs
@@ -5,37 +5,28 @@ using UnityEngine;
 namespace PitGan.ScreenShake {
 	public class ScreenShaker : MonoBehaviour {
 
-		/// <summary>
-		/// number of frames until shaking stops
-		/// </summary>
-		private float shakeTimer;
+		private int shakeTimer;
+		private int shakeWaitTimer = 2;
+		private float radius;
+		private Vector3 currentDisplacement;
 
-		/// <summary>
-		/// the number of frames it waits before shaking
-		/// </summary>
-		private int shakeVigor = 30;
-
-		private int radius;
 		private static int FullCircleInDegrees = 360;
 
-		public int ShakeVigor {
-			get { return shakeVigor; }
-			set { shakeVigor = value; }
-		}
-
 		private void OnPreRender() {
-			if (shakeTimer > 0 && shakeTimer % shakeVigor == 0) {
+			if (shakeTimer > 0) {
 				shakeTimer--;
-				MoveCamera();
+				if (shakeTimer % shakeWaitTimer == 0) {
+					NewPos();
+				}
+				transform.position += currentDisplacement;
 			}
 		}
 
-		private void MoveCamera() {
-			Vector2 newPos = Random.insideUnitCircle * radius;
-			transform.localPosition += (Vector3)newPos;
+		private void NewPos() {
+			this.currentDisplacement = Random.insideUnitCircle * radius;
 		}
 
-		public void Shake(int shakeTime, int shakeRadius) {
+		public void Shake(int shakeTime, float shakeRadius) {
 			this.shakeTimer = shakeTime;
 			this.radius = shakeRadius;
 		}
